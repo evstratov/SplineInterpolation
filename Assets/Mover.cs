ac503bd4f390969abc446e2f6f25f31bb8f34450
Kirill Evstratov <evstratovkirill@yandex.ru>
2021-08-10 18:18:06 +0300

    Интерполяция сплайнами

---
+++ Assets/Mover.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform P0;
+    [SerializeField] private Transform P1;
+    [SerializeField] private Transform P2;
+    [SerializeField] private Transform P3;
+    [SerializeField] private float speed;
+    
+    [Range(0, 1)]
+    public float t;
+
+    void Update()
+    {
+        transform.position = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, t);
+        transform.rotation = Quaternion.LookRotation(Bezier.GetFirstDerivative(P0.position, P1.position, P2.position, P3.position, t));
+    }
+
+
+    private void OnDrawGizmos()
+    {
+
+        int sigmentsNumber = 20;
+        Vector3 preveousePoint = P0.position;
+
+        for (int i = 0; i < sigmentsNumber + 1; i++)
+        {
+            float paremeter = (float)i / sigmentsNumber;
+            Vector3 point = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, paremeter);
+            Gizmos.DrawLine(preveousePoint, point);
+            preveousePoint = point;
+        }
+
+    }
+}
